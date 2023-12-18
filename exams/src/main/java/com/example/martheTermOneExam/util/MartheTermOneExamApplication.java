8384b3b08ebb8664eb84ed00ff6fcbc86e0792b9
MartheUwir <marthuwir@gmail.com>
2023-12-18 10:00:34 +0200

    this is Marthe's work

---
+++ exams/src/main/java/com/example/martheTermOneExam/util/MartheTermOneExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.marthetermoneexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MartheTermOneExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MartheTermOneExamApplication.class, args);
+    }
+
+}
