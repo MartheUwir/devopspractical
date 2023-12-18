b448e73e0aaacfa6c8db0f343b3359349100f37c
MartheUwir <marthuwir@gmail.com>
2023-12-18 10:07:52 +0200

    this is Marthe's work

---
+++ exams/src/test/java/com/example/martheTermOneExam/util/MartheTermOneExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.marthetermoneexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MartheTermOneExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
